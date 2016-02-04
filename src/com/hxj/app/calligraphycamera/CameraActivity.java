1f64de6d1753832a9c29990426d5428ee4cef815
Hxuejie <hxuejie@126.com>
2016-02-05 00:28:44 +0800

    添加拍照功能和照片保存（未完成）

--- src/com/hxj/app/calligraphycamera/CameraActivity.java
+++ src/com/hxj/app/calligraphycamera/CameraActivity.java
@@ -1,6 +1,7 @@
 package com.hxj.app.calligraphycamera;
 
 import java.io.IOException;
+import java.util.List;
 
 import com.hxj.app.calligraphycamera.apicloud.APICloudAdapterActivity;
 import com.koushikdutta.urlimageviewhelper.UrlImageViewCallback;
@@ -9,19 +10,29 @@ import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Paint.Align;
+import android.graphics.Rect;
 import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.Size;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -37,11 +48,15 @@ public class CameraActivity extends APICloudAdapterActivity {
 	private SurfaceView			cameraView;
 	private ImageView			wordView;
 	private View				colorView;
+	private TextView			watermarkView;
 
 	private Camera				camera;
 	private Bitmap				wordImage;
+	private Bitmap				filterWordImage;
 	private int					filterColor;
 	private String				wordURL;
+	private int					photoSize	= 500;
+	private Bitmap				photo;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +64,7 @@ public class CameraActivity extends APICloudAdapterActivity {
 		setContentView("camera_activity");
 		cameraView = (SurfaceView) findViewById("camnera_cameraview");
 		wordView = (ImageView) findViewById("camera_word");
+		watermarkView = (TextView) findViewById("camera_watermark");
 		colorView = findViewById("camear_color");
 		initView();
 
@@ -68,6 +84,11 @@ public class CameraActivity extends APICloudAdapterActivity {
 	private void initView() {
 		filterColor = Color.WHITE;
 		colorView.setBackgroundColor(filterColor);
+
+		Display display = this.getWindowManager().getDefaultDisplay();
+		int width = display.getWidth();
+		LayoutParams lp = cameraView.getLayoutParams();
+		lp.height = width;
 	}
 
 	@Override
@@ -110,14 +131,14 @@ public class CameraActivity extends APICloudAdapterActivity {
 	public void onCameraOperation(View v) {
 		// 关闭
 		if (camera != null) {
-			closeCamera();
+			takePhoto();
 			return;
 		}
 
 		// 1.创建过滤图片
 		if (wordImage != null) {
-			Bitmap img = colorFilter(wordImage, filterColor, COLOR_RANGE);
-			wordView.setImageBitmap(img);
+			filterWordImage = colorFilter(wordImage, filterColor, COLOR_RANGE);
+			wordView.setImageBitmap(filterWordImage);
 		} else {
 			tip(getString("no_image"));
 		}
@@ -127,18 +148,103 @@ public class CameraActivity extends APICloudAdapterActivity {
 	}
 
 	/**
+	 * 拍照
+	 */
+	private void takePhoto() {
+		if (camera == null) {
+			return;
+		}
+		debug("take photo");
+		camera.takePicture(null, null, new PictureCallback() {
+			@Override
+			public void onPictureTaken(byte[] data, Camera camera) {
+				photo = BitmapFactory.decodeByteArray(data, 0, data.length);
+				debug("photo size: " + photo.getWidth() + " x "
+						+ photo.getHeight());
+				Bitmap mixPicture = mixPictures();
+				savePicture(mixPicture);
+			}
+		});
+	}
+
+	private void savePicture(Bitmap img) {
+		MediaStore.Images.Media.insertImage(getContentResolver(), img, "title",
+				"description");
+	}
+
+	/**
+	 * 混合图片
+	 * 
+	 * @return
+	 */
+	private Bitmap mixPictures() {
+		if (photo == null) {
+			return null;
+		}
+		// 创建透明图片
+		int w = photoSize;
+		int h = photoSize;
+		Bitmap ret = Bitmap.createBitmap(w, h, Config.ARGB_8888);
+		Canvas c = new Canvas(ret);
+		// 画底图
+		int pw = photo.getWidth();
+		int ph = photo.getHeight();
+		int padding = (ph - pw) / 2;
+		Rect src = new Rect(0, padding, pw, ph - padding);
+		Rect dst = new Rect(0, 0, w, h);
+		Paint p = new Paint();
+		p.setAntiAlias(true);
+		c.drawBitmap(photo, src, dst, p);
+		// 画字
+		if (filterWordImage != null) {
+			int left = (w - filterWordImage.getWidth()) / 2;
+			int top = (h - filterWordImage.getHeight()) / 2;
+			c.drawBitmap(filterWordImage, left, top, p);
+		}
+		// 画水印
+		String watermark = watermarkView.getText().toString();
+		p.setTextAlign(Align.RIGHT);
+		c.drawText(watermark, w, h, p);
+		return ret;
+
+	}
+
+	/**
 	 * 打开相机
 	 */
 	private void openCamera() {
 		debug("opean camera");
 		try {
 			camera = Camera.open(0);
+			Parameters parameters = camera.getParameters();
+			parameters.setJpegQuality(100);
+			List<Size> list = parameters.getSupportedPictureSizes();
+			Size size = selectPhotoSize(list, photoSize);
+			debug("set photo size: " + size.width + " x " + size.height);
+			parameters.setPictureSize(size.width, size.height);
+			camera.setParameters(parameters);
 			camera.setPreviewDisplay(cameraView.getHolder());
 			camera.setDisplayOrientation(90);
 			camera.startPreview();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * 选择照片尺寸
+	 * 
+	 * @param list
+	 * @param baseSize
+	 * @return
+	 */
+	private Size selectPhotoSize(List<Size> list, int baseSize) {
+		for (Size size : list) {
+			if (size.width >= baseSize && size.height >= baseSize) {
+				return size;
+			}
+		}
+		return list.get(0);
 	}
 
 	/**
@@ -195,12 +301,12 @@ public class CameraActivity extends APICloudAdapterActivity {
 		// 过滤颜色
 		for (int r = 0; r < h; ++r) {
 			for (int l = 0; l < w; ++l) {
-				int px = src.getPixel(l, r);
+				int px = retImg.getPixel(l, r);
 				if (colorEqual(px, filterColor, colorRange)) {
-					src.setPixel(l, r, Color.TRANSPARENT);
+					retImg.setPixel(l, r, Color.TRANSPARENT);
 				} else {
 
-					src.setPixel(l, r, 0x90FFFFFF & px);
+					retImg.setPixel(l, r, 0x90FFFFFF & px);
 				}
 			}
 		}
@@ -249,7 +355,7 @@ public class CameraActivity extends APICloudAdapterActivity {
 	 * 下载字体图片
 	 */
 	private void downloadWordImage() {
-		if (!TextUtils.isEmpty(wordURL)) {
+		if (TextUtils.isEmpty(wordURL)) {
 			return;
 		}
 		debug("download word image: " + wordURL);
